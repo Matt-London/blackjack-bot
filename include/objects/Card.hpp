7a2695f759850182d2405f20b05adcaf3782d5a4
Matt London <mattlondon57@gmail.com>
2020-06-28 12:20:20 -0400

    Fill out basic Card

--- include/objects/Card.hpp
+++ include/objects/Card.hpp
@@ -1,11 +1,44 @@
 #ifndef CARD_GUARD
 #define CARD_GUARD
 
+#include <iostream>
+
 class Card {
     private:
-
+        char suit;
+        char number;
+        char color;
 
     public:
+        // =============== Constructors ===============
+        // Default
+        Card();
+
+        // Takes suit, num
+        Card(char number, char suit);
+
+        // =============== Getters ===============
+        char get_suit();
+        char get_number();
+        char get_color();
+
+
+        // =============== Setters ===============
+        void set_suit(char suit);
+        void set_number(char number);
+
+
+        // =============== Methods ===============
+        // Copies from another card
+        void copy_from(Card a);
+        
+        // Calculates color from the provided suit
+        void calculate_color();
+
+        // =============== Debug ===============
+        // Print card
+        void print_card();
+
 
 };
 
--- src/objects/Card.cpp
+++ src/objects/Card.cpp
@@ -1,1 +1,67 @@
 #include "../../include/objects/Card.hpp"
+// =============== Constructors ===============
+// Default
+Card::Card() {
+    number = '0';
+    suit = '0';
+    color = '0';
+
+}
+
+// Takes suit, num
+Card::Card(char number, char suit) {
+    this->number = number;
+    this->suit = suit;
+    Card::calculate_color();
+
+}
+
+// =============== Getters ===============
+char Card::get_suit() {
+    return suit;
+}
+char Card::get_number() {
+    return number;
+}
+char Card::get_color() {
+    return color;
+}
+
+
+// =============== Setters ===============
+void Card::set_suit(char suit) {
+    this->suit = suit;
+    Card::calculate_color();
+}
+void Card::set_number(char number) {
+    this->number = number;
+}
+
+
+// =============== Methods ===============
+// Copies from another card
+void Card::copy_from(Card a) {
+    this->suit = a.get_suit();
+    this->number = a.get_number();
+    Card::calculate_color();
+
+}
+
+// Calculates color from the provided suit
+void Card::calculate_color() {
+    if (suit == 'h' || suit == 'd') {
+        color = 'r';
+    }
+    else if (suit == 'c' || suit == 's') {
+        color = 'b';
+    }
+    else {
+        color = '0';
+    }
+}
+
+// =============== Debug ===============
+// Print card
+void Card::print_card() {
+    std::cout << number << ' ' << suit << std::endl;
+}
