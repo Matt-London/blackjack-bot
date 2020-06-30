105193e93041ad659ea0ec28ac943aadf296a2f6
Matt London <mattlondon57@gmail.com>
2020-06-30 12:09:16 -0400

    Build basic deck function

--- include/objects/Deck.hpp
+++ include/objects/Deck.hpp
@@ -1,13 +1,45 @@
 #ifndef DECK_GUARD
 #define DECK_GUARD
 
+#include <vector>
+
 #include "Card.hpp"
+
+using namespace std;
 
 class Deck {
     private:
 
+
     public:
-    
+        vector<Card> deck;
+
+
+        // ============= Constructors =============
+        // Default, makes full ordered deck
+        Deck();
+
+        // Debug, makes blank deck
+        Deck(bool b);
+
+        // ============= Getters =============
+        int get_size();
+
+        // ============= Setters =============
+
+
+        // ============= Methods =============
+        // Adds given card
+        void add_card(Card c);
+        
+        // Builds default deck
+        void build_deck();
+
+        // ============= Debug =============
+
+
+
+
 };
 
 #endif
--- src/objects/Deck.cpp
+++ src/objects/Deck.cpp
@@ -1,1 +1,46 @@
 #include "../../include/objects/Deck.hpp"
+
+// ============= Constructors =============
+// Default, makes full ordered deck
+Deck::Deck() {
+    Deck::build_deck();
+}
+
+// Debug, makes blank deck
+Deck::Deck(bool b) {
+
+}
+
+// ============= Getters =============
+int Deck::get_size() {
+    return deck.size();
+}
+
+// ============= Setters =============
+
+
+// ============= Methods =============
+// Adds given card
+void Deck::add_card(Card c) {
+    deck.push_back(c);
+}
+
+// Builds default deck
+void Deck::build_deck() {
+    deck.clear();
+    deck.reserve(52);
+
+    // Loop through suits
+    for (int i = 1; i < 5; i++) {
+        // Loop through ranks
+        for (int j = 1; j < 14; j++) {
+            // Add cards to deck
+            deck.emplace_back(j, i);
+        }
+    }
+
+    deck.shrink_to_fit();
+
+}
+
+// ============= Debug =============
